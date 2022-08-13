a8c722912a7ba802a3001cb9acf080d08270d334
altescy <altescy@fastmail.com>
2022-08-13 18:09:53 +0900

    update example

--- examples/square.py
+++ examples/square.py
@@ -1,13 +1,27 @@
+import logging
+
 from cachestore import Cache
+
+logging.basicConfig(level=logging.INFO)
 
 cache = Cache()
 
 
 @cache()
 def square(x: int) -> int:
-    print(f"square is executed with {x}")
+    print(f"square is executed with {x}!")
+    return x * x
+
+
+@cache(expire=-1)
+def expired_square(x: int) -> int:
+    print(f"expired_square is executed with {x}")
     return x * x
 
 
 if __name__ == "__main__":
-    print(square(2))
+    print(f"{square(2)=}")
+    print(f"{square(2)=}")
+    print(f"{square(3)=}")
+    print(f"{expired_square(2)=}")
+    print(f"{expired_square(2)=}")

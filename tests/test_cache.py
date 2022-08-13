80403ff34ddce462918aa4de26be4c70b8415170
altescy <altescy@fastmail.com>
2022-08-13 16:47:06 +0900

    add test for cache

---
+++ tests/test_cache.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from cachestore import Cache, LocalStorage
+
+
+def test_cache(tmp_path: Path) -> None:
+    cache_root = tmp_path / "cache"
+    cache = Cache(LocalStorage(cache_root))
+
+    @cache()
+    def square(x: int) -> int:
+        return x * x
+
+    output_1 = square(2)
+    assert cache.exists(square)
+
+    output_2 = square(2)
+    assert output_1 == output_2 == 4
